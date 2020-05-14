b00737b3363f2c49a60ada9ba543dd16f9fc3c07
toru <t02f447f@gmail.com>
2020-05-14 18:12:05 +0900

    ADD app.js for add , delete, search, toggleDone

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -293,25 +293,108 @@ var _vue2 = _interopRequireDefault(_vue);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+// https://jp.vuejs.org/v2/examples/todomvc.html
+var STORAGE_KEY = 'vue-todo';
+var todoStorage = {
+  fetch: function fetch() {
+    var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+    todos.forEach(function (todo, index) {
+      todo.id = index;
+    });
+    todoStorage.uid = todos.length;
+    return todos;
+  },
+  save: function save(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  }
+};
+
 new _vue2.default({
-  el: '.list',
+  el: '#app',
   data: {
     lists: [{
       id: 1,
-      text: テキストテキスト１,
+      text: 'テキストテキスト１',
       isDone: false,
       editMode: false
     }, {
       id: 2,
-      text: テキストテキスト２,
-      isDone: false,
+      text: 'テキストテキスト２',
+      isDone: true,
       editMode: false
     }, {
       id: 3,
-      text: テキストテキスト３,
+      text: 'テキストテキスト３',
       isDone: false,
       editMode: false
-    }]
+    }],
+    isShowAdd: false,
+    searchWord: ''
+
+  },
+  methods: {
+    // 入力欄表示
+    showAdd: function showAdd() {
+      this.isShowAdd = !this.isShowAdd;
+    },
+
+    // リスト追加
+    addList: function addList(e) {
+      // 入力したリストの要素を取得
+      var text = this.$refs.text;
+      // 空欄の時は何もしない
+      if (!text.value) {
+        return;
+      }
+
+      // データを追加
+      this.lists.push({
+        id: todoStorage.uid++,
+        text: text.value,
+        isDone: false,
+        editMode: false
+      });
+
+      // 入力欄をクリア
+      text.value = "";
+      this.isShowAdd = false;
+    },
+
+    // リスト削除
+    rmList: function rmList(list) {
+      var index = this.lists.indexOf(list);
+      this.lists.splice(index, 1);
+    },
+
+    // 完了と未完了切替
+    toggleDone: function toggleDone(list) {
+      list.isDone = !list.isDone;
+    }
+  },
+  computed: {
+    searchLists: function searchLists() {
+      var _this = this;
+
+      return this.lists.filter(function (elm) {
+        var regexp = new RegExp('^' + _this.searchWord, 'i');
+        return elm.text.match(regexp);
+      });
+    }
+  },
+  watch: {
+    lists: {
+      // 引数はウォッチしているプロパティの変更後の値
+      handler: function handler(lists) {
+        todoStorage.save(lists);
+      },
+
+      // deepオプションでネストしているデータも監視できる
+      deep: true
+    }
+  },
+  created: function created() {
+    // インスタンス生成時に自動的にfetchする
+    this.lists = todoStorage.fetch();
   }
 });
 
